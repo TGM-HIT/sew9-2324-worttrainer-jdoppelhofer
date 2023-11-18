0853a663be8a5f4db7506b831ccaa1f3d67c7903
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2023-11-18 19:13:24 +0100

    Controller implementierung

--- app/src/main/java/sew9/worttrainer/jdoppelhofer/Controller.java
+++ app/src/main/java/sew9/worttrainer/jdoppelhofer/Controller.java
@@ -1,17 +1,33 @@
 package sew9.worttrainer.jdoppelhofer;
 
+/**
+ * Diese Klasse ist der Controller des MVC-Patterns.
+ * @author Julian Doppelhofer
+ */
 public class Controller {
     private Rechtsschreibtrainer rechtsschreibtrainer;
     private GUI gui;
+    private Speichern speichern;
 
-    public Controller(Rechtsschreibtrainer rechtsschreibtrainer) {
+    public Controller(Rechtsschreibtrainer rechtsschreibtrainer, Speichern speichern) {
         this.rechtsschreibtrainer = rechtsschreibtrainer;
+        this.speichern = speichern;
     }
 
+    /**
+     * Überprüft das eingegebene Wort.
+     * @param wort Das eingegebene Wort
+     */
     public void check(String wort) {
-        if (rechtsschreibtrainer.raten(wort)) {
-            gui.updateImage(rechtsschreibtrainer.getAktuellesWort());
+        boolean richtig = rechtsschreibtrainer.raten(wort);
+        if (!richtig) {
+            rechtsschreibtrainer.wortWaehlen(); // Zum nächsten Wort wechseln, wenn falsch geraten
         }
+        gui.updateImage(rechtsschreibtrainer.getAktuellesWort());   // Bild aktualisieren
+        // Statistik aktualisieren
+        gui.updateStatistik(rechtsschreibtrainer.getInsgeamtWorte(),
+                rechtsschreibtrainer.getRichtigeWorte(),
+                rechtsschreibtrainer.getFalscheWorte());
     }
 
     public Wort getWort() {
@@ -20,5 +36,24 @@ public class Controller {
 
     public void setGUI(GUI gui) {
         this.gui = gui;
+    }
+
+    public void speichern() {
+        speichern.speichern(rechtsschreibtrainer);
+    }
+
+    /**
+     * Lädt den Rechtsschreibtrainer.
+     */
+    public void laden() {
+        Rechtsschreibtrainer geladenerTrainer = speichern.laden();
+        if (geladenerTrainer != null) {
+            this.rechtsschreibtrainer = geladenerTrainer;
+            // Aktualisieren Sie die GUI mit den neuen Daten
+            gui.updateImage(rechtsschreibtrainer.getAktuellesWort());
+            gui.updateStatistik(rechtsschreibtrainer.getInsgeamtWorte(),
+                    rechtsschreibtrainer.getRichtigeWorte(),
+                    rechtsschreibtrainer.getFalscheWorte());
+        }
     }
 }
