8a38cc704edeba44fea00d7ed69b37f57e00da11
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2023-11-18 19:12:18 +0100

    Rechtsschreibtrainer implementierung

--- app/src/main/java/sew9/worttrainer/jdoppelhofer/Rechtsschreibtrainer.java
+++ app/src/main/java/sew9/worttrainer/jdoppelhofer/Rechtsschreibtrainer.java
@@ -5,11 +5,12 @@ import java.util.Set;
 
 /**
  * Diese Klasse beinhaltet die Wörter und die Statistik des Rechtsschreibtrainers.
+ * @author Julian Doppelhofer
  */
 public class Rechtsschreibtrainer {
     private Wort[] woerter; //Array mit allen Wörtern
     private Wort aktuellesWort; //Das aktuelle Wort
-    private Set<Integer> ausgewaehlteWoerter;
+    private Set<Integer> ausgewaehlteWoerter; // Set mit allen bereits ausgewählten Wörtern
     private int insgesamtWorte; //Anzahl aller geratenen Wörter
     private int richtigeWorte; //Anzahl aller richtig geratenen Wörter
     private int falscheWorte; //Anzahl aller falsch geratenen Wörter
@@ -54,6 +55,7 @@ public class Rechtsschreibtrainer {
      * @return true, wenn das Wort richtig geraten wurde, sonst false
      */
     public boolean raten(String wort) {
+        insgesamtWorte++; // Statistik für jeden Versuch aktualisieren
         if (wort == null || wort.isEmpty())
             throw new IllegalArgumentException();
         if (wort.equals(aktuellesWort.getWort())) {
@@ -65,7 +67,39 @@ public class Rechtsschreibtrainer {
         return false;
     }
 
+    /**
+     * Gibt das aktuelle Wort zurück.
+     * @return Das aktuelle Wort
+     */
     public Wort getAktuellesWort() {
         return aktuellesWort;
+    }
+
+    public Wort[] getWoerter() {
+        return woerter;
+    }
+
+    public int getInsgeamtWorte() {
+        return insgesamtWorte;
+    }
+
+    public int getRichtigeWorte() {
+        return richtigeWorte;
+    }
+
+    public int getFalscheWorte() {
+        return falscheWorte;
+    }
+
+    /**
+     * Setzt die Statistik.
+     * @param insgesamt Anzahl aller geratenen Wörter
+     * @param richtig Anzahl aller richtig geratenen Wörter
+     * @param falsch Anzahl aller falsch geratenen Wörter
+     */
+    public void setStatistik(int insgesamt, int richtig, int falsch) {
+        this.insgesamtWorte = insgesamt;
+        this.richtigeWorte = richtig;
+        this.falscheWorte = falsch;
     }
 }
